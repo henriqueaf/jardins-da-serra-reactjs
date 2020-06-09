f0ee385295891ab80f87fb870457e168ad627294
Henrique Almeida <henrique_af1@msn.com>
2020-06-09 01:25:40 -0300

    Remove routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-import Routes from './routes';
+// import Routes from './routes';
+import Map from './pages/Map';
 
 function App() {
-  return <Routes />;
+  return <Map />;
 }
 
 export default App;
