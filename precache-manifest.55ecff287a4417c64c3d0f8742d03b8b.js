7d3c614d6cde279d4818b80701b0ad812cddf84d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-15 18:05:27 +0000

    Deploy henriqueaf/jardins-da-serra-reactjs to github.com/henriqueaf/jardins-da-serra-reactjs.git:gh-pages

---
+++ precache-manifest.55ecff287a4417c64c3d0f8742d03b8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7c47b2b925f0aee4d06a78274b3252",
+    "url": "/jardins-da-serra-reactjs/index.html"
+  },
+  {
+    "revision": "538d327b73a6c2fd1179",
+    "url": "/jardins-da-serra-reactjs/static/css/main.9753e00a.chunk.css"
+  },
+  {
+    "revision": "38a960993a6cb171d310",
+    "url": "/jardins-da-serra-reactjs/static/js/2.5aaae712.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/jardins-da-serra-reactjs/static/js/2.5aaae712.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "538d327b73a6c2fd1179",
+    "url": "/jardins-da-serra-reactjs/static/js/main.a2694121.chunk.js"
+  },
+  {
+    "revision": "fd3d63f6854dac4fdb3e",
+    "url": "/jardins-da-serra-reactjs/static/js/runtime-main.66aa3cbd.js"
+  }
+]);
